93461defca750ffc2442c22851a3ac84e01dc367
Daniel Rodolfo Marquez <marquez@linprog7.cs.fsu.edu>
2018-09-08 15:41:23 -0400

    Functioning Tokenizer

--- Bash.c
+++ Bash.c
@@ -23,22 +23,22 @@
 void prompt( ){
  
  char user[33];					//max linux user length is 32
- char  machine[HOST_NAME_MAX];			//max machine name
+ char  machine[HOST_NAME_MAX + 1];			//max machine name
  char cwd[PATH_MAX + 1];			//max Path length
  int len;
 
- strcpy( machine, getenv("MACHINE"));		//get machine string
+// strcpy( machine, getenv("MACHINE"));		//get machine string
  strcpy(user, getenv("USER"));			//get user string
  getcwd(cwd, PATH_MAX);				//get current working directory
- len  = strlen(user) + strlen(machine) + strlen(cwd) + 10;
+ len  = 32 + HOST_NAME_MAX + PATH_MAX + 10;
 
  char *prompt = malloc( len * sizeof(char) );	//Allocate space for the prompt string
  
- strcpy(prompt, user);
+ strcpy(prompt, getenv("USER"));
  strcat(prompt,"@");
- strcat(prompt,machine);
+ strcat(prompt,getenv("MACHINE"));
  strcat(prompt," :: ");
- strcat(prompt,cwd);
+ strcat(prompt,getcwd(cwd,PATH_MAX));
  strcat(prompt," ->");
  printf("%s", prompt);
  free(prompt);
@@ -47,102 +47,104 @@ void prompt( ){
 
 //This function takes an array of pointers and an array of chars
 //Parses char array and adds address of each token to array of pointers
+//returns the number of tokens in the argument
 //
 //Written by Daniel
+//I would like to point out that I wrote a mjority of this function
+//for a similar project in UnixTools
+//But the code is 100% mine
 
-void tokenize( char ** arg, char *line){	//Function still needs work
- 
+int tokenize( char ** arg, char *line){	
  int i = 0;
  int it = 0;
+ char temp;
  while( line[i] != '\0' ){
-	if( !isspace( line[i] ) && line[i] != '\'' && line[i] != '\"'){
-		arg[it] = &line[i]; 
-		it ++;
-		i ++;
-		while( !isspace( line[i] ) )
-			++i;	
-		}
-
-	else if( line[i] == '\'' ){
-		++i;
-		if (line[i] != '\''){
-			arg[it] = &line[i];
-			++i;
-			++it;
-			while(line[i] != '\''){
-				if(line[i] == '\n'){
-					printf( "Unmatched '.\n");
-					return;
-					}
-				else ++i;
+ 	if ( !isspace( line[i] ) && line[i] != '\'' && line[i] != '\"'){
+        	arg[it] = &line[i];      
+                ++it;
+                ++i;
+                while( i < 50 ){
+                	if( isspace( line[i])){
+                        	line[i] = '\0';
+                                 break;
+                                 }
+                         else if( line[i] == '\0' )
+                                 return it;
+                                 ++i;
+                         }
+                }
+        else if( line[i] == '\''){
+                 ++i;
+                 arg[it] = &line[i];
+                 ++it;
+                 while( 1 ){
+        	         if( line[i] == '\'' ){
+                	         line[i] = '\0';
+                                 break;
+                                 }
+			 else if( line[i] == '\0' ){
+				printf("Unmatched '.\n");
+				return -1;
 				}
-			}
-		++i;
-		}
-	else ++i;
-	}
+                          ++i;
+                          }
+                 }
+		else if( line[i] == '\"'){
+                        ++i;
+                        arg[it] = &line[i];
+                        ++it;
+                        while( 1 ){
+                                if( line[i] == '\"' ){
+                                        line[i] = '\0';
+                                        break;
+                                        }
+				else if( line[i] == '\0' ){
+                                	printf("Unmatched \".\n");
+                                	return -1;
+					}
+                                ++i;
+                                }
+                        }
+                ++i;
+                }
+        return it;
+
  }
 		
-//Returns the number of tokens in command
-//
-//Written by Daniel
 
-int token_count(char * line){			//Not returning the correct number
-	int it = 0;				//still needs work
-	int count = 0;
-	while(line[it] != '\0'){
-	 if (line[it] == '\''){
-		++count;
-		++it;
-		while(line[it] != '\'')
-			++it;
-		++it;
-		}
-
-	 else if (line[it] == '\"'){
-                ++count;
-                ++it;
-                while(line[it] != '\"')
-                        ++it;
-                ++it;
-                }
-
-	 else if( !isspace( line[it] ) ){
-		++count;
-		++it;
-		while( !isspace( line[it] ) )
-			it++;
-		++it;
-		}
-
-	 else ++it;
-	 }
- return count;
- }
-			
 
 int main(){
   
 	char command[256];
-	char **argv = {NULL};
+	char ** argv;
 	int x;
 
  while(1){
 	prompt();
 	strcpy(command,readline(" "));
- 	x = 0;
+ 	//printf("%s\n",command);
+	if( !strcmp(command, "exit") )
+		break;
+
+
+	printf("#%s#\n",command);
+	argv = malloc( 10 * sizeof(char*) );	
+	for(x = 0; x < 10; ++x)				//loop to intialize pointers to NULL
+		argv[x] = NULL;
 	
-	printf("%i\n", token_count(command));
-	printf("%i\n", token_count(command));
-	argv = malloc( token_count(command) * sizeof(char*) );	
-		
-	tokenize(argv,command);
-	//print(argv);
+	x = tokenize(argv,command);			
+	printf("%i\n",x);				
 	
-	if( strcpy(command, "exit") )
-		exit(EXIT_SUCCESS);
+	//for(x = 0; argv[x] != NULL; ++x)		//loop to print arguments
+	//	printf("#%s# ", argv[x]);
+	//printf("\n");
+		//print(argv);
+	
+
 	
 	}
+
+ exit(EXIT_SUCCESS);
 
 
 
