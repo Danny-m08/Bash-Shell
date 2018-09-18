42af740ff62c47d02a49ad0df12d46306a7a2083
Danny-m08 <marquez@linprog7.cs.fsu.edu>
2018-09-18 14:57:14 -0400

    Functioning io redirections except for when both are present

--- Bash.c
+++ Bash.c
@@ -20,158 +20,7 @@
 //
 //Written By Daniel
 
-void prompt( ){
- int len;
- len  = 32 + HOST_NAME_MAX + PATH_MAX + 10;
 
- char *prompt = malloc( len * sizeof(char) );	//Allocate space for the prompt string
- 
- strcpy(prompt, getenv("USER"));
- strcat(prompt,"@");
- strcat(prompt,getenv("MACHINE"));
- strcat(prompt," :: ");
- strcat(prompt,getcwd(NULL,PATH_MAX));
- strcat(prompt," ->");
- printf("%s", prompt);
- free(prompt);
- return;
-}
-
-//This function takes an array of pointers and an array of chars
-//Parses char array and adds address of each token to array of pointers
-//returns the number of tokens in the argument
-//
-//Written by Daniel
-//I would like to point out that I wrote a mjority of this function
-//for a similar project in UnixTools
-//But the code is 100% mine
-
-int tokenize( char ** arg, char *line, char  *out_redir, char  *in_redir){	
- int i = 0;
- int it = 0;
- char temp;
-
-
- while( line[i] != '\0' ){
-  out_redir = in_redir = NULL;
-
-	if( line[i] == '>' ){
-		line[i] = '\0';
-		while( isspace( line[++i] ))
-			line[i] = '\0';
-		out_redir = &line[i];
-		while( !isspace(line[++i]) && line[i] != '\0')
-                        continue;
-                line[i] = '\0';
-		}
-	
-	else if( line[i] == '<' ){
-		line[i] = '\0';
-		while( isspace( line[++i] ) )
-			line[i] = '\0';
-		in_redir = &line[i];	
-		while( !isspace(line[++i]) && line[i] != '>')
-                       continue;
-		if(line[i] != '>')
-	                line[i] = '\0';
-		}
-
-
-	else if ( !isspace( line[i] ) && line[i] != '\'' && line[i] != '\"' ){
-		if( in_redir != NULL || out_redir != NULL )
-			continue;
-					
-        	arg[it] = &line[i];      
-                ++it;
-                //+i;
-                while( 1 ){
-			++i;		
-			if( line[i] == '<'){
-				line[i] = '\0';	
-							
-				while( isspace(line[++i]))
-					line[i] = '\0';
-				in_redir = & line[i];
-				while( !isspace(line[++i]) && line[i] != '>' )
-					continue;
-				line[i] = '\0';
-				while( isspace(line[++i]))
-                                        line[i] = '\0';
-                                out_redir = & line[i];
-                                while( !isspace(line[++i] ))
-                                        continue;
-                                line[i] = '\0';
-				break;
-				}
-			else if (line[i] == '>'){
-				line[i] = '\0';
-                                while( isspace(line[++i]))
-                                        line[i] = '\0';
-                                out_redir = & line[i];
-				while( !isspace(line[++i] ))
-                                        continue;
-                                line[i] = '\0';
-				break;
-				}
-                	else if( isspace( line[i])){
-                        	line[i] = '\0';
-                                 break;
-                                 }
-                         else if( line[i] == '\0' )
-                                 return it;
-                                 
-                         }
-		//++i;
-                }
-        else if( line[i] == '\''){
-                 arg[it] = &line[i];
-		 ++i;
-                 ++it;
-                 while( 1 ){
-        	         if( line[i] == '\'' ){
-                	         line[i] = '\0';
-                                 break;
-                                 }
-			 else if( line[i] == '\0' ){
-				printf("Unmatched '.\n");
-				return -1;
-				}
-                          ++i;
-                          }
-                 }
-	else if( line[i] == '\"'){
-               
-                arg[it] = &line[i];
-		++i;               
-		++it;
-                 while( 1 ){
-        	         if( line[i] == '\"' ){
-                	         line[i] = '\0';
-                                 break;
-                                 }
-			 else if( line[i] == '\0' ){
-                               	printf("Unmatched \".\n");
-                               	return -1;
-				}
-                        ++i;
-                        }
-                  }
-	else if( isspace(line[i]) )
-		line[i] = '\0';
-	
-	 ++i;
-         }
-  
- printf("file ouput redirect: %s$\nfile input redirect:%s$\n", out_redir, in_redir);      
- return it;
- }
-
-<<<<<<< HEAD
-
-
-
-
-=======
 char *expandPath(char *path, int cmd)
 {
         char * tempPath; // = malloc(strlen(path) * sizeof(char));
@@ -234,23 +83,21 @@ char *expandPath(char *path, int cmd)
                                 free(tempPath);
                         }
                 }
-        }
+if((expPath = strstr(path, ".")) != NULL)
+                {
+                        //printf("relative to curr directory");
+                        expPath++;                      //gets rid of ./ at the beginning 
+                        //printf("this is exp Path: %s \n", expPath);
+                        tempPath = malloc(strlen(getenv("PWD")));
+                        strcpy(tempPath, getenv("PWD"));
+                        tempPath = realloc(tempPath, strlen(tempPath) + strlen(expPath));
+                        strcat(tempPath, expPath);
+                        //printf("this is temppath: %s \n", tempPath);
+                        strcpy(path, tempPath);
+                        free(tempPath);
+                }
 
-	if((expPath = strstr(path, ".")) != NULL)
-		{
-			//printf("relative to curr directory");
-			expPath++;                      //gets rid of ./ at the beginning 
-			//printf("this is exp Path: %s \n", expPath);
-			tempPath = malloc(strlen(getenv("PWD")));
-			strcpy(tempPath, getenv("PWD"));
-			tempPath = realloc(tempPath, strlen(tempPath) + strlen(expPath));
-			strcat(tempPath, expPath);
-			//printf("this is temppath: %s \n", tempPath);
-			strcpy(path, tempPath);
-			free(tempPath);
-		}
-		
-	//expand the ~ 
+        //expand the ~ 
         if((expPath = strstr(path, "~")) != NULL)
         {
                 printf("contains ~ \n");
@@ -284,7 +131,7 @@ char *expandPath(char *path, int cmd)
                 case 2:                                                         // this is built in 
                         printf("Case 2, its built in %s \n", path);
                         return path;
-			break;
+                        break;
                 case 3:
                         // need to loop through each case and see if its there or not
                         // have to find in path
@@ -298,7 +145,7 @@ char *expandPath(char *path, int cmd)
                                 varLocation++;                          // takes out this colon
                                 //printf("varLocation: %s\n", varLocation);
                                 temp = calloc(tempSize + strlen(path) + 2, sizeof(char));
-                                //printf("malloc vs strlen(should be zero): %i vs %i",tempSize + strlen(path) +2, strlen(temp));
+                                                                                              //printf("malloc vs strlen(should be zero): %i vs %i",tempSize + strlen(path) +2, strlen(temp));
                                 strncpy(temp, tempPath,tempSize);
                                 strcat(temp,"/");
                                 strcat(temp, path);
@@ -333,41 +180,134 @@ char *expandPath(char *path, int cmd)
                                 return temp;
                         }
                         break;
->>>>>>> 04b9357fae57b482faf3b1eb5bf2c106630ce89d
+//>>>>>>> 04b9357fae57b482faf3b1eb5bf2c106630ce89d
+}
+}
 
-int findoutputRedirect( char ** argv ){
-	int x;
-	for( x = 0; argv[x] != NULL; ++x){
-		if( !strcmp(">",argv[x]) )
-			return x;
+}
+
+
+
+
+void prompt( ){
+ int len;
+ len  = 32 + HOST_NAME_MAX + PATH_MAX + 10;
+
+ char *prompt = malloc( len * sizeof(char) );	//Allocate space for the prompt string
+ 
+ strcpy(prompt, getenv("USER"));
+ strcat(prompt,"@");
+ strcat(prompt,getenv("MACHINE"));
+ strcat(prompt," :: ");
+ strcat(prompt,getcwd(NULL,PATH_MAX));
+ strcat(prompt," ->");
+ printf("%s", prompt);
+ free(prompt);
+ return;
+}
+
+//This function takes an array of pointers and an array of chars
+//Parses char array and adds address of each token to array of pointers
+//returns the number of tokens in the argument
+//
+//Written by Daniel
+//I would like to point out that I wrote a mjority of this function
+//for a similar project in UnixTools
+//But the code is 100% mine
+
+int tokenize( char ** arg, char *line, char **out_redir, char **in_redir){	
+ int i = 0;
+ int it = 0;
+ char temp;
+
+
+ while( line[i] != '\0' ){
+  *out_redir = *in_redir = NULL;
+
+	if( line[i] == '>' ){
+		line[i] = '\0';
+		while( isspace( line[++i] ))
+			continue;
+		*out_redir = &line[i];
+		 while( ++i){
+                        if( line[i] == '\0' || isspace(line[++i])){
+                                --i;
+                                break;}
+			}
 		}
-	}
+	
+	else if( line[i] == '<' ){
+		line[i] = '\0';
+		while( isspace( line[++i] ) )
+			continue;
+		*in_redir = &line[i];	
+		printf("%c\n",**in_redir);
 
-
-
-
-int findinputRedirect( char ** argv ){
-	int x;
-	for( x = 0; argv[x] != NULL; ++x){
-		if( !strcmp(">", argv[x]) )
-			return x;
+		while( ++i ){
+                	if(line[i] == '>' || line[i] == '\0' || isspace(line[i])){ 
+				--i;
+				break;}
+			}
+			   
 		}
-	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	else if ( !isspace( line[i] ) && line[i] != '\'' && line[i] != '\"' ){
+						
+        	arg[it] = &line[i];      
+                ++it;
+                
+                while( ++i ){
+					
+			if( line[i] == '<' || line[i] == '>' || isspace(line[i]) || line[i] == '\0'){
+				--i;
+				break;	
+				}			
+		      	                                 
+                         }
+		
+                }
+        else if( line[i] == '\''){
+                 arg[it] = &line[i];
+		 ++i;
+                 ++it;
+                 while( 1 ){
+        	         if( line[i] == '\'' ){
+                	         line[i] = '\0';
+                                 break;
+                                 }
+			 else if( line[i] == '\0' ){
+				printf("Unmatched '.\n");
+				return -1;
+				}
+                          ++i;
+                          }
+                 }
+	else if( line[i] == '\"'){
+               
+                arg[it] = &line[i];
+		++i;               
+		++it;
+                 while( 1 ){
+        	         if( line[i] == '\"' ){
+                	         line[i] = '\0';
+                                 break;
+                                 }
+			 else if( line[i] == '\0' ){
+                               	printf("Unmatched \".\n");
+                               	return -1;
+				}
+                        ++i;
+                        }
+                  }
+	else if( isspace(line[i]) )
+		line[i] = '\0';
+	
+	 ++i;
+         }
+  
+ return it;
+ }
 
 
 
@@ -403,7 +343,6 @@ int main(){
         int x, status;
 	pid_t child_id, temp_id;
         
-	int pipe, redir;
 	int fd1, fd2;
 
         argv = malloc( 10 * sizeof(char*) );            //Still haven't figured out max # of tokens
@@ -421,7 +360,9 @@ int main(){
       for(x = 0; x < 10; ++x)
 		argv[x] = NULL;
 
-        x = tokenize(argv,command, out_file, in_file);                     
+        x = tokenize(argv,command, &out_file, &in_file);                    
+	printf("out_file: %s\n", out_file);
+	printf("in_file: %s\n",in_file);
 	for(x=0; argv[x] != NULL; ++x )
 		printf("%s\n", argv[x]);
 
@@ -457,14 +398,14 @@ int main(){
 		else if(argv[x][0] == '\'')
 			printf("%s ", &argv[x][1]);
 
-		else if(argv[x][0] == '\"'){
+		//else if(argv[x][0] == '\"'){
 	//		char * temp[10] = {NULL};
 	//		for(x = 0; x < 10; ++x)
 	//			temp[x] = NULL;
 	//		tokenize(temp, &argv[x][1]);
 			//for(x=0; temp[x] != NULL; ++x)
 			//	printf("%s\n", *temp[x]);
-			}
+		//	}
 		else printf("%s ", argv[x] );
 	            }
 	printf("\n");
@@ -504,20 +445,16 @@ int main(){
 		}
 
 
+	
 
-        else if( (child_id = fork() ) == 0 ){
-		
-		//for (x = 0; x < 10; ++x)
-		//	arg2[x] = NULL;
-
-             	if( out_file != NULL && in_file != NULL )
-			x = x;
-		else if (out_file != NULL ){
-			fd1 = open( out_file, O_CREAT | O_WRONLY);
-			if( fd1!= -1 )
+       else if( (child_id = fork() ) == 0 ){
+           	if (out_file != NULL ){
+			fd1 = open( out_file, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR);
+			//if( fd1!= -1 )
 				dup2(fd1, 1);
+			printf("redirecting to %s\n", out_file);
 			}
-		else if( in_file != NULL ){
+		if( in_file != NULL ){
 			fd1 = open( in_file, O_RDONLY );
 			if( fd1 != -1 )
 				dup2(fd1, 0);
@@ -532,7 +469,7 @@ int main(){
                 printf("Unknown command %s\n", argv[0] );	//
                 }
 	else{
-	
+	//	printf("child_id: %i\n",child_id);
 	 while(1){
 							//wait for child process to return
 		x = waitpid(child_id, &status, 0);	//parent process code`
